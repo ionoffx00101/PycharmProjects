76122f2eb7961ceee3fe724e2ca68fa9ef452484
teQoo <anes2354@gmail.com>
2017-03-17 17:57:11 +0900

    solve 041

---
+++ untitled/sambaek/041_050/042.py
@@ -0,0 +1,11 @@
+dic = [[['순위','1'],['영화','닥터스트레인지']],[["순위","2"],["영화","닥터이상한"]],[["순위","3"],["영화","이상한의사"]]]
+
+dic['순위']
+
+# littleDic = dic['순위']
+#
+# for i in littleDic:
+#     print(littleDic[i])
+
+# print(dic['순위'])
+# TypeError: list indices must be integers or slices, not str
